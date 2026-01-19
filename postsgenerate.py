f73c4e499a7ac88dae7069e1a4738704c3740359
abdalsamieali <abdalsamieali67@gmail.com>
2026-01-19 23:06:02 +0200

    adding post 3

--- postsgenerate.py
+++ postsgenerate.py
@@ -7,7 +7,7 @@ from jinja2 import *
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('posts_template.html')
 
-index = int(sys.argv[1])
+index = int(sys.argv[1]) 
 
 with open('data.json', 'r') as data:
     context = json.load(data)
@@ -16,5 +16,5 @@ context.update({'index': index})
 
 content = template.render(context)
 
-with open(f"posts/no{index}.html", 'w') as f:
+with open(f"posts/no{index +1}.html", 'w') as f:
     f.write(content)
